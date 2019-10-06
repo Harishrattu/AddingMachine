58c15523e5d2796a9f4b085abdc7c1198ed35016
Harish <hrattu@asu.edu>
2019-10-06 15:38:47 -0700

    cse360 assignment2.

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * @return the current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * adds the parameter to the total value
+	 * @param value any new int value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * subtracts the parameter to the total value
+	 * @param value any new int value
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/*
+	 * prints the history of transactions
+	 */
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
