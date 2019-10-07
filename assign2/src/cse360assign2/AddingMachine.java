c733f88b1ec46887e741eee63b1f0ee79c705a37
Harish <hrattu@asu.edu>
2019-10-06 19:25:10 -0700

    Finished the code.

--- assign2/src/cse360assign2/AddingMachine.java
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -1,8 +1,15 @@
+/*
+ * Name: Harish Rattu
+ * Date: 10/06/19
+ * URL: https://github.com/Harishrattu/AddingMachine.git
+ */
+
 package cse360assign2;
 
 public class AddingMachine {
 
 	private int total;
+	private String output;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
@@ -11,31 +18,37 @@ public class AddingMachine {
 	/*
 	 * @return the current total
 	 */
-	public int getTotal () {
-		return 0;
+	public int getTotal () 
+	{
+		return total;
 	}
 	
 	/*
 	 * adds the parameter to the total value
 	 * @param value any new int value
 	 */
-	public void add (int value) {
-		
+	public void add (int value)
+	{
+		total = total + value;
+		output = output + " + " + value;
 	}
 	
 	/*
 	 * subtracts the parameter to the total value
 	 * @param value any new int value
 	 */
-	public void subtract (int value) {
-		
+	public void subtract (int value) 
+	{
+		total = total - value;
+		output = output + " - " + value;
 	}
 		
 	/*
 	 * prints the history of transactions
 	 */
-	public String toString () {
-		return "";
+	public String toString () 
+	{
+		return "0" + output;
 	}
 
 	public void clear() {
